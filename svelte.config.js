82d0b79d2e647a3602c6c9eb6ed24a279c79aa47
Jus1ine <157552272+Jus1ine@users.noreply.github.com>
2024-10-06 22:03:49 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,17 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter({ pages: "build", assets: "build", fallback: undefined, precompress: false, strict: true }),
-        prerender: {
-            handleHttpError: 'warn', // Options: 'warn', 'continue', 'ignore'
-        }
-    },
+	kit: {
+		adapter: adapter({
+			pages: "build", // Ensure this matches your upload path
+			assets: "build",
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+	},
 };
 
 export default config;
